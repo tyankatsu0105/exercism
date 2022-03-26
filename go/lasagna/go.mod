a6728336260b1642a22777967fcdcf5f8da386d4
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2022-03-27 00:30:45 +0900

    add lasagna

---
+++ go/lasagna/go.mod
@@ -0,0 +1,3 @@
+module github.com/tyankatsu0105/exercism/go/lasagna
+
+go 1.16
---
+++ go/lasagna/lasagna.go
@@ -0,0 +1,22 @@
+package lasagna
+
+// OvenTime is time for using oven.
+var OvenTime int = 40
+
+// RemainingOvenTime returns the remaining minutes based on the `actual` minutes already in the oven.
+func RemainingOvenTime(actualMinutesInOven int) int {
+	return OvenTime - actualMinutesInOven
+}
+
+// PreparationTime calculates the time needed to prepare the lasagna based on the amount of layers.
+func PreparationTime(numberOfLayers int) int {
+	return numberOfLayers * 2
+}
+
+// ElapsedTime calculates the total time needed to create and bake a lasagna.
+func ElapsedTime(numberOfLayers, actualMinutesInOven int) int {
+	layer := PreparationTime(numberOfLayers)
+	minute := actualMinutesInOven
+
+	return layer + minute
+}
