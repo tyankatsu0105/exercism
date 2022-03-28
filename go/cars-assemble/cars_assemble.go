c5140e47bb47b956d4d76eb77d8200884a6c7464
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2022-03-28 22:54:13 +0900

    fmt cars-assemble

--- go/cars-assemble/cars_assemble.go
+++ go/cars-assemble/cars_assemble.go
@@ -2,7 +2,7 @@ package cars
 
 import "math"
 
-func percentage(number float64) float64  {
+func percentage(number float64) float64 {
 	return number / 100
 }
 
@@ -15,14 +15,14 @@ func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float6
 // CalculateWorkingCarsPerMinute calculates how many working cars are
 // produced by the assembly line every minute
 func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
-	var expression float64 =(float64(productionRate)/60) * (successRate/100)
+	var expression float64 = (float64(productionRate) / 60) * (successRate / 100)
 
 	return int(math.Floor(expression))
 }
 
 // CalculateCost works out the cost of producing the given number of cars
 func CalculateCost(carsCount int) uint {
-	var tensPlace, onesPlace = carsCount / 10, carsCount%10
+	var tensPlace, onesPlace = carsCount / 10, carsCount % 10
 
-	return uint(tensPlace)*95000 + uint(onesPlace) * 10000
+	return uint(tensPlace)*95000 + uint(onesPlace)*10000
 }
