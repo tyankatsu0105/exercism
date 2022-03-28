f9c35542e153369106b069daa1a0211352bf0b0f
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2022-03-28 22:50:19 +0900

    add cars-assemble

---
+++ go/cars-assemble/cars_assemble.go
@@ -0,0 +1,28 @@
+package cars
+
+import "math"
+
+func percentage(number float64) float64  {
+	return number / 100
+}
+
+// CalculateWorkingCarsPerHour calculates how many working cars are
+// produced by the assembly line every hour
+func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float64 {
+	return (float64(productionRate) / 100) * successRate
+}
+
+// CalculateWorkingCarsPerMinute calculates how many working cars are
+// produced by the assembly line every minute
+func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
+	var expression float64 =(float64(productionRate)/60) * (successRate/100)
+
+	return int(math.Floor(expression))
+}
+
+// CalculateCost works out the cost of producing the given number of cars
+func CalculateCost(carsCount int) uint {
+	var tensPlace, onesPlace = carsCount / 10, carsCount%10
+
+	return uint(tensPlace)*95000 + uint(onesPlace) * 10000
+}
---
+++ go/cars-assemble/go.mod
@@ -0,0 +1,3 @@
+module github.com/tyankatsu0105/exercism/go/cars-assemble
+
+go 1.16
