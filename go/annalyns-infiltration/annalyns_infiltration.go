955eab391ba8f867f8e38428f43be1233a452499
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2022-03-28 22:16:04 +0900

    refactor annalyn

--- go/annalyns-infiltration/annalyns_infiltration.go
+++ go/annalyns-infiltration/annalyns_infiltration.go
@@ -2,41 +2,45 @@ package annalyn
 
 // CanFastAttack can be executed only when the knight is sleeping
 func CanFastAttack(knightIsAwake bool) bool {
-	if knightIsAwake == false {
-		return true
-	}
+	return !knightIsAwake
+	// if knightIsAwake == false {
+	// 	return true
+	// }
 
-	return false
+	// return false
 }
 
 // CanSpy can be executed if at least one of the characters is awake
 func CanSpy(knightIsAwake, archerIsAwake, prisonerIsAwake bool) bool {
-	if knightIsAwake == true || archerIsAwake == true || prisonerIsAwake == true {
-		return true
-	}
+	return knightIsAwake || archerIsAwake || prisonerIsAwake
+	// if knightIsAwake == true || archerIsAwake == true || prisonerIsAwake == true {
+	// 	return true
+	// }
 
-	return false
+	// return false
 }
 
 // CanSignalPrisoner can be executed if the prisoner is awake and the archer is sleeping
 func CanSignalPrisoner(archerIsAwake, prisonerIsAwake bool) bool {
-	if archerIsAwake == false && prisonerIsAwake == true {
-		return true
-	}
+	return !archerIsAwake && prisonerIsAwake
+	// if archerIsAwake == false && prisonerIsAwake == true {
+	// 	return true
+	// }
 
-	return false
+	// return false
 }
 
 // CanFreePrisoner can be executed if the prisoner is awake and the other 2 characters are asleep
 // or if Annalyn's pet dog is with her and the archer is sleeping
 func CanFreePrisoner(knightIsAwake, archerIsAwake, prisonerIsAwake, petDogIsPresent bool) bool {
-	if archerIsAwake == false && petDogIsPresent == true {
-		return true
-	}
+	return (!archerIsAwake && petDogIsPresent) || (!knightIsAwake && !archerIsAwake && prisonerIsAwake)
+	// if archerIsAwake == false && petDogIsPresent == true {
+	// 	return true
+	// }
 
-	if knightIsAwake == false && archerIsAwake == false && prisonerIsAwake == true {
-		return true
-	}
+	// if knightIsAwake == false && archerIsAwake == false && prisonerIsAwake == true {
+	// 	return true
+	// }
 
-	return false
+	// return false
 }
