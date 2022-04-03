199c1478a62cd6ae44ee0d3de98c2425d0ff5be0
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2022-04-04 00:09:40 +0900

    add partyrobot

---
+++ go/party-robot/party_robot.go
@@ -0,0 +1,22 @@
+package partyrobot
+
+import (
+	"fmt"
+)
+
+// Welcome greets a person by name.
+func Welcome(name string) string {
+	return "Welcome to my party, " + name + "!"
+}
+
+// HappyBirthday wishes happy birthday to the birthday person and exclaims their age.
+func HappyBirthday(name string, age int) string {
+	return "Happy birthday " + name + "! " + "You are now " + fmt.Sprint(age) + " years old!"
+}
+
+// AssignTable assigns a table to each guest.
+func AssignTable(name string, table int, neighbor, direction string, distance float64) string {
+	return Welcome(name) + "\n" + 
+	"You have been assigned to table " + fmt.Sprintf("%03d", table) + ". " + "Your table is " + direction + ", " + "exactly " + fmt.Sprintf("%.1f", distance) +  " meters from here." + "\n" + 
+	"You will be sitting next to " + neighbor + "."
+}
