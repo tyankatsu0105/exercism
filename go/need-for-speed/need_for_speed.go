367bfe3468bc9a3f420259c1fc173e4a9612a943
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2022-05-04 00:47:36 +0900

    add need-for-speed

---
+++ go/need-for-speed/need_for_speed.go
@@ -0,0 +1,63 @@
+package speed
+
+import "math"
+
+// Car is the properties of a car.
+type Car struct {
+	battery      int
+	batteryDrain int
+	speed        int
+	distance     int
+}
+
+// NewCar creates a new remote controlled car with full battery and given specifications.
+func NewCar(speed, batteryDrain int) Car {
+	const initialValueBattery = 100
+	const initialValueDistance = 0
+
+	return Car{
+		battery:      initialValueBattery,
+		distance:     initialValueDistance,
+		speed:        speed,
+		batteryDrain: batteryDrain,
+	}
+}
+
+// Track is the properties of a track.
+type Track struct {
+	distance int
+}
+
+// NewTrack created a new track
+func NewTrack(distance int) Track {
+	return Track{
+		distance: distance,
+	}
+}
+
+// Drive drives the car one time. If there is not enough battery to drive on more time,
+// the car will not move.
+func Drive(car Car) Car {
+	remainingAmountOfGasoline := car.battery - car.batteryDrain
+	canDrive := remainingAmountOfGasoline > 0
+
+	if !canDrive {
+		return car
+	}
+
+	car.distance = car.distance + car.speed
+	car.battery = remainingAmountOfGasoline
+
+	return car
+}
+
+// CanFinish checks if a car is able to finish a certain track.
+func CanFinish(car Car, track Track) bool {
+	battery := float64(car.battery)
+	distancePerBattery := float64(car.speed) / float64(car.batteryDrain)
+	drivableDistance := math.Floor(distancePerBattery * battery)
+
+	distance := float64(track.distance)
+
+	return drivableDistance >= distance
+}
